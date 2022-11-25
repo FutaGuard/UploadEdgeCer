ec5a946c6d4bddc5763e8cd78105055e5792d538
踢低吸 <tdc@tdccc.com.tw>
2022-11-25 03:54:23 +0000

    ⚡️ 編輯/更新 SSL

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
-import requests
+from requests import Session
 import config
 from typing import List
 from json.decoder import JSONDecodeError
 
+class Notify:
+    def __init__(self) -> None:
+        self.endpoint = f'https://api.telegram.org/bot{config}/'
+        self.client: Session = Session()
+
+    def send_message(self, chat_id: int, text: str):
+        payload = {'chat_id': chat_id, 'text': text}
+        url = self.endpoint + 'sendMessage'
+        self.client.post(url, json=payload)
 
 class CFAPI:
-    def __init__(self) -> None:
+    def __init__(self, client: Session) -> None:
         self.endpoint: str = 'https://api.cloudflare.com/client/v4/zones/{zone}/'
         self.domains: List[str] = config.DOMAINS
+        self.client = client
 
-    def certificate_packs_list_certificate_packs(self, domain: str) -> dict:
+    def certificate_packs_list_certificate_packs(self, zoneid: str) -> dict:
+        """_summary_
+
+        Args:
+            domain (str): _description_
+
+        Returns:
+            dict: _description_
+        """
         url = self.endpoint + 'ssl/certificate_packs'
-        r = requests.get(url)
+        r = self.client.get(url.format(zone=zoneid))
         if not r.ok:
             print('list cert error')
             print(r.text)
@@ -20,4 +38,51 @@ class CFAPI:
             return r.json()
         except JSONDecodeError:
             print('list cert parse JSON err')
-            
+
+    def edit_ssl(self, zoneid: str, domain: str, certid: str) -> dict:
+        url = self.endpoint + 'custom_certificates/{certid}'
+        prikey = open(
+            f'/home/ubuntu/.acme.sh/*.{domain}/*.{domain}.key', 'r').read()
+        cert = open(
+            f'/home/ubuntu/.acme.sh/*.{domain}/fullchain.cer', 'r').read()
+
+        payload = {
+            'certificate': cert,
+            'private_key': prikey,
+            'bundle_method': 'ubiquitous',
+            'type': 'sni_custom'
+        }
+        r = self.client.patch(url.format(
+            zone=zoneid, certid=certid), data=payload)
+        if not r.ok:
+            print('edit ssl error')
+            return
+
+        try:
+            return r.json()
+        except JSONDecodeError:
+            print('edit ssl parse JSON err')
+
+
+if __name__ == '__main__':
+    client = Session()
+    client.headers = {
+        'Authorization': config.CF_TOKEN}
+    cf = CFAPI(client)
+    bot = Notify()
+
+    for domain in config.DOMAINS:
+        zoneid = config.DOMAINS[domain]
+        result = cf.certificate_packs_list_certificate_packs(zoneid)
+
+        certlist = list(
+            filter(lambda x: x['type'] == 'legacy_custom', result['result']))
+        if len(certlist) != 1:
+            pass
+        else:
+            certid = certlist[0]['id']
+            r = cf.edit_ssl(zoneid=zoneid, certid=certid, domain=domain)
+            if not r:
+                text = '❌ SSL 上傳失敗\n' \
+                       '域名：{domain}'.format(domain=domain)
+                bot.send_message(config.CHAT_ID, text)
